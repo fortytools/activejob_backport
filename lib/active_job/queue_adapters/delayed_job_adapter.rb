90e52214a3df84ee7866eefe4f869303627bb208
Sebastian M. Gauck <smg@42ls.de>
2017-08-01 12:23:55 +0200

    provide job_data for delayed_job wrapper

--- lib/active_job/queue_adapters/delayed_job_adapter.rb
+++ lib/active_job/queue_adapters/delayed_job_adapter.rb
@@ -3,21 +3,35 @@ require 'delayed_job'
 module ActiveJob
   module QueueAdapters
     class DelayedJobAdapter
+
       class << self
-        def enqueue(job)
-          JobWrapper.new.delay(queue: job.queue_name).perform(job.serialize)
+        def enqueue(job) #:nodoc:
+          delayed_job = Delayed::Job.enqueue(JobWrapper.new(job.serialize), queue: job.queue_name, priority: job.priority)
+          job.provider_job_id = delayed_job.id
+          delayed_job
         end
 
-        def enqueue_at(job, timestamp)
-          JobWrapper.new.delay(queue: job.queue_name, run_at: Time.at(timestamp)).perform(job.serialize)
+        def enqueue_at(job, timestamp) #:nodoc:
+          delayed_job = Delayed::Job.enqueue(JobWrapper.new(job.serialize), queue: job.queue_name, priority: job.priority, run_at: Time.at(timestamp))
+          job.provider_job_id = delayed_job.id
+          delayed_job
         end
       end
 
-      class JobWrapper
-        def perform(job_data)
+      class JobWrapper #:nodoc:
+        attr_accessor :job_data
+
+        def initialize(job_data)
+          @job_data = job_data
+        end
+
+        def perform
           Base.execute(job_data)
         end
       end
     end
   end
 end
+
+
+
